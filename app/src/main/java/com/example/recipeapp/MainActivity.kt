20bb198ddc46150adf8a44661f83455024ade752
ADarknessSoul <a21110179@ceti.mx>
2023-04-17 21:49:24 -0600

    Agregué estilos a los componentes

--- app/src/main/java/com/example/recipeapp/MainActivity.kt
+++ app/src/main/java/com/example/recipeapp/MainActivity.kt
@@ -2,10 +2,38 @@ package com.example.recipeapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.LinearLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var btnLightMode: Button
+        btnLightMode = findViewById(R.id.view_togglebutton)
+
+        btnLightMode.setOnClickListener(object : View.OnClickListener {
+
+            override fun onClick(view: View?) {
+
+                changeMode(btnLightMode)
+
+            }
+
+        })
+
     }
+
+    fun changeMode(btnLightMode: Button) {
+
+        var backgroundLayer: LinearLayout = findViewById(R.id.mainLayout)
+
+        //if(btnLightMode.isActivated) backgroundLayer.setBackgroundColor()
+
+    }
+
+
+
 }
